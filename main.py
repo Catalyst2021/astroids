1fc6eb8f2053a061c472d1c56b2682e7a43fd8ab
Catalyst2021 <Anddrew.Le@gmail.com>
2026-04-30 10:55:14 -0400

    add FPS control to game set to 60fpsand cleaned up code

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from logger import log_state
 
 def main():
     pygame.init()
-    fps = pygame.time.Clock()
-    dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 
 
     while True: 
         log_state()
@@ -16,10 +16,10 @@ def main():
                 return 
             
         screen.fill("black")
-
-
         pygame.display.flip()
-        dt = fps.tick(60) / 1000
+
+        # limit the framerate to 60 FPS
+        dt = clock .tick(60) / 1000
 
             
 if __name__ == "__main__":
