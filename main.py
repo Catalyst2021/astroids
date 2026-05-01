7233588ae908011b7e56d76a50ef9ba719989ace
Catalyst2021 <Anddrew.Le@gmail.com>
2026-05-01 15:33:44 -0400

    ch4 l1:asteroids

--- main.py
+++ main.py
@@ -3,38 +3,41 @@ from circleshape import CircleShape
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0 
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    asteroidfields = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
+    dt = 0 
     while True: 
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 return 
-        
+            
+        updatable.update(dt) 
+
         #Make screen black
         screen.fill("black")
 
-        updatable.update(dt) 
-        for draws in drawable: 
-            draws.draw(screen)
-            
-        #Draw player 
-        #player.update(dt)
-        #player.draw(screen) 
-        
-        
-           
-
+        for obj in drawable: 
+            obj.draw(screen)    
     
         #Refresh the screen
         pygame.display.flip()
